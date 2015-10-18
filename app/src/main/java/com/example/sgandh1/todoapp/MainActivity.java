d8a41bf2c10b2c1b883d0232c7edd6bfdc55aa72
Shreyansh Gandhi <sgandhi@walmartlabs.com>
2015-10-18 16:42:57 -0700

    walkthrough complete

--- app/src/main/java/com/example/sgandh1/todoapp/MainActivity.java
+++ app/src/main/java/com/example/sgandh1/todoapp/MainActivity.java
@@ -2,16 +2,24 @@ package com.example.sgandh1.todoapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<String> items;
-    ArrayAdapter<String> itemsAdapter;
+    ArrayList<String> todoItems;
+    ArrayAdapter<String> aToDoAdapter;
     ListView lvItems;
+    EditText etEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +27,51 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         lvItems = (ListView) findViewById(R.id.lvItems);
-        items = new ArrayList<String>();
-        items.add("one");
-        items.add("two");
-        items.add("three");
+        etEditText = (EditText) findViewById(R.id.etEditText);
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                todoItems.remove(position);
+                aToDoAdapter.notifyDataSetChanged();
+                writeItems();
+                return true;
+            }
+        });
 
-        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        readItems();
+        aToDoAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, todoItems);
 
-        lvItems.setAdapter(itemsAdapter);
+        lvItems.setAdapter(aToDoAdapter);
 
+    }
+
+
+    private void readItems() {
+        File filesDir = getFilesDir();
+        File file = new File(filesDir, "todo.txt");
+
+        try {
+            todoItems = new ArrayList<String>(FileUtils.readLines(file));
+        } catch (IOException e) {
+            e.printStackTrace();
+            todoItems = new ArrayList<String>();
+        }
+    }
+
+    private void writeItems() {
+        File filesDir = getFilesDir();
+        File file = new File(filesDir, "todo.txt");
+
+        try {
+            FileUtils.writeLines(file, todoItems);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onAddItem(View view) {
+        aToDoAdapter.add(etEditText.getText().toString());
+        etEditText.setText("");
+        writeItems();
     }
 }
